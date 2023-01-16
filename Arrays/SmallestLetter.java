338568c23f9194303ba71bd4f7f70f0bec84ceae
Pushpendra gour <101712869+pushpendragour1@users.noreply.github.com>
2023-01-16 23:27:56 +0530

    Add files via upload

---
+++ Arrays/SmallestLetter.java
@@ -0,0 +1,33 @@
+package Binarysearch;
+
+public class SmallestLetter {
+ 
+   public static void main(String[] args) {
+    char ch[] = {'c','j','m'};
+    char target ='c';
+    char ans = nextGreatestLetter(ch , target);
+    System.out.println(ans);
+
+   }
+        public static char nextGreatestLetter(char[] letters, char target) {
+            int start =0;
+            int end = letters.length-1;
+    
+            while(start<=end)
+            {
+                int mid =  start + (end - start)/2;
+    
+                if(letters[mid] > target)
+                {
+                    end = mid -1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            } 
+   return letters[start % letters.length];
+        }
+    
+    
+}
